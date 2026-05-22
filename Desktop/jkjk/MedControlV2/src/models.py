70f2547a908b11060aabd68c4982d7ede0518be6
Arthur Lopes Oliveira <129803691+Arthurestak@users.noreply.github.com>
2026-05-22 13:20:17 -0300

    Add MedicationService for managing medications

--- Desktop/jkjk/MedControlV2/src/models.py
+++ Desktop/jkjk/MedControlV2/src/models.py
@@ -1,16 +1,27 @@
-class Medication:
-    def __init__(self, name, time, taken=False):
-        self.name = name
-        self.time = time
-        self.taken = taken
-
-    def to_dict(self):
-        return {
-            "name": self.name,
-            "time": self.time,
-            "taken": self.taken
-        }
-
-    @staticmethod
-    def from_dict(data):
-        return Medication(data["name"], data["time"], data["taken"])
+from src.storage import load, save_new, update_taken
+from src.models import Medication
+ 
+ 
+class MedicationService:
+    def __init__(self):
+        self.medications = load()
+ 
+    def add(self, name: str, time: str) -> None:
+        if not name:
+            raise ValueError("Nome inválido")
+ 
+        med = Medication(name, time)
+        db_id = save_new(med)
+        med.db_id = db_id
+        self.medications.append(med)
+ 
+    def list(self) -> list[Medication]:
+        return self.medications
+ 
+    def mark_as_taken(self, index: int) -> None:
+        if index < 0 or index >= len(self.medications):
+            raise ValueError("Índice inválido")
+ 
+        med = self.medications[index]
+        med.taken = True
+        update_taken(med.db_id, True)
