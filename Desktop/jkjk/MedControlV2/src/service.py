770e3c10b10f4d30781050ed77f55f72a599ec7c
Arthur Lopes Oliveira <129803691+Arthurestak@users.noreply.github.com>
2026-05-22 13:18:51 -0300

    Refactor MedicationService methods for type hints

--- Desktop/jkjk/MedControlV2/src/service.py
+++ Desktop/jkjk/MedControlV2/src/service.py
@@ -1,26 +1,27 @@
-from src.storage import load, save
+from src.storage import load, save_new, update_taken
 from src.models import Medication
-
-
+ 
+ 
 class MedicationService:
     def __init__(self):
         self.medications = load()
-
-    def add(self, name, time):
+ 
+    def add(self, name: str, time: str) -> None:
         if not name:
             raise ValueError("Nome inválido")
-
+ 
         med = Medication(name, time)
+        db_id = save_new(med)
+        med.db_id = db_id
         self.medications.append(med)
-
-        save(self.medications)
-
-    def list(self):
+ 
+    def list(self) -> list[Medication]:
         return self.medications
-
-    def mark_as_taken(self, index):
+ 
+    def mark_as_taken(self, index: int) -> None:
         if index < 0 or index >= len(self.medications):
             raise ValueError("Índice inválido")
-
-        self.medications[index].taken = True
-        save(self.medications)
+ 
+        med = self.medications[index]
+        med.taken = True
+        update_taken(med.db_id, True)
